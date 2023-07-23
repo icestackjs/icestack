1e88685b88300904691a52beef78d65b4ca3671d
ice breaker <1324318532@qq.com>
2023-07-24 01:11:26 +0800

    chore: commt async getCss

--- packages/ui/scripts/util.ts
+++ packages/ui/scripts/util.ts
@@ -2,27 +2,11 @@ import postcss from 'postcss'
 import tailwindcss from 'tailwindcss'
 
 export async function getCss() {
-  const { css } = await postcss([
+  const res = await postcss([
     tailwindcss({
       content: [{ raw: '' }]
-      // theme: {
-      //   extend: {
-      //     // inherit: 'inherit',
-      //     // current: 'currentColor',
-      //     // transparent: 'transparent',
-      //     // black: '#000',
-      //     // white: '#fff',
-      //     primary: 'rgb(var(--primary) / <alpha-value>)',
-      //     success: 'rgb(var(--success) / <alpha-value>)',
-      //     error: 'rgb(var(--error) / <alpha-value>)',
-      //     warning: 'rgb(var(--warning) / <alpha-value>)',
-      //     'primary-content': 'rgb(var(--primary-color) / <alpha-value>)'
-      //   }
-      // },
-      // corePlugins: {
-      //   preflight: false
-      // }
     })
     // @ts-ignore
   ]).process('@tailwind base;\n@tailwind components;\n@tailwind utilities;\n}')
+  return res
 }

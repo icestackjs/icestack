9751fe24126b0325d7838d5ab2d4fb0ee6f2be70
ice breaker <1324318532@qq.com>
2023-07-27 23:50:39 +0800

    feat: commit codes

--- apps/taro-app/postcss.config.cjs
+++ apps/taro-app/postcss.config.cjs
@@ -1,7 +1,12 @@
-// object.keys顺序
+// object.keys顺序 https://www.npmjs.com/package/postcss-load-config
+// https://github.com/postcss/postcss-load-config#ordering
 module.exports = {
-  plugins: [
-    require('@pandacss/dev/postcss')(),
-    require('@csstools/postcss-cascade-layers')()
-  ]
+  plugins: {
+    '@pandacss/dev/postcss': {},
+    '@csstools/postcss-cascade-layers': {}
+  }
+  // [
+  //   require('@pandacss/dev/postcss')(),
+  //   require('@csstools/postcss-cascade-layers')()
+  // ]
 }

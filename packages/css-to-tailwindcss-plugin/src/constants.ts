c64a36a9538b673a580e6d1290317529fd50a6bc
ice breaker <1324318532@qq.com>
2023-09-28 01:24:42 +0800

    chore: package.json resolve version

--- packages/css-to-tailwindcss-plugin/src/constants.ts
+++ packages/css-to-tailwindcss-plugin/src/constants.ts
@@ -5,10 +5,12 @@ export const layerNodes = {
   // variants: null
 }
 
-export const packageJson = require('../package.json')
+// export const packageJson = require('../package.json')
 
-export const version = packageJson.version as string
+// packageJson.version as string
 
 export const markedLayerKey = Symbol('__tw_layer_name__')
 
 export const layerNodesKeys = Object.keys(layerNodes)
+
+export { version } from '../package.json'

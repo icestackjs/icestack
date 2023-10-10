7b5a56702c9d3adcade2d2d81533202f23d3d21b
ice breaker <1324318532@qq.com>
2023-10-10 14:19:23 +0800

    chore: remove asset dist

---
+++ packages/theme-algorithm/src/default/colorAlgorithm.ts
@@ -0,0 +1,8 @@
+import { TinyColor } from '@ctrl/tinycolor'
+
+export const getAlphaColor = (baseColor: string, alpha: number) => new TinyColor(baseColor).setAlpha(alpha).toRgbString()
+
+export const getSolidColor = (baseColor: string, brightness: number) => {
+  const instance = new TinyColor(baseColor)
+  return instance.darken(brightness).toHexString()
+}

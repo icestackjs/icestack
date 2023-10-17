6fd0952c7a283f5bc6d1c518a1f1a9aba68348f4
ice breaker <1324318532@qq.com>
2023-10-17 18:09:42 +0800

    chore: commit codes

--- packages/ui/src/unocss.ts
+++ packages/ui/src/unocss.ts
@@ -1,10 +1,10 @@
-import { Preset, definePreset } from 'unocss'
-import base from '../assets/js/base/index.js'
-import components from '../assets/js/components/index.js'
-import utilities from '../assets/js/utilities/index.js'
+import { definePreset } from '@unocss/core'
+// import base from '../assets/js/base/index.js'
+// import components from '../assets/js/components/index.js'
+// import utilities from '../assets/js/utilities/index.js'
 
-import { colors } from './colors.js'
-import { groupBy } from './utils'
+// import { colors } from './colors.js'
+// import { groupBy } from './utils'
 export interface PresetOptions {}
 
 export default definePreset((options?: PresetOptions) => {

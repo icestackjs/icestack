95e0789c53de7d59772b4c0d5571f318a065eb8f
ice breaker <1324318532@qq.com>
2023-10-24 16:52:57 +0800

    chore: remove some compoents

--- packages/ui/src/components/checkbox.ts
+++ packages/ui/src/components/checkbox.ts
@@ -20,7 +20,21 @@ const colorsMap = expandColorsMap(Types, (cur) => {
 })
 const defaults: IDefaults = {
   styled: {
-    default: 'border-base-content rounded-btn h-6 w-6 cursor-pointer appearance-none border border-opacity-20'
+    default: 'border-base-content rounded-btn h-6 w-6 cursor-pointer appearance-none border border-opacity-20',
+    checked: {
+      apply: 'bg-base-content bg-no-repeat',
+      css: {
+        animation: 'checkmark var(--animation-input, 0.2s) ease-out'
+      }
+    },
+    indeterminate: {
+      apply: 'bg-base-content bg-no-repeat',
+      css: {
+        animation: 'checkmark var(--animation-input, 0.2s) ease-out'
+      }
+    },
+    disabled: 'bg-base-content cursor-not-allowed border-transparent opacity-20',
+    focusVisible: 'outline-base-content outline outline-2 outline-offset-2'
   }
 }
 // const injectName = createInjectName('checkbox')
--- packages/ui/src/sass/functions.ts
+++ packages/ui/src/sass/functions.ts
@@ -4,13 +4,13 @@ import { get } from 'lodash'
 import type allComponents from '../allComponents'
 import { transformJsToSass } from './utils'
 import * as avatar from '@/components/avatar'
-import * as bottomNavigation from '@/components/bottom-navigation'
+// import * as bottomNavigation from '@/components/bottom-navigation'
 import * as button from '@/components/button'
 import * as alert from '@/components/alert'
 import * as badge from '@/components/badge'
 import * as chat from '@/components/chat'
 import * as checkbox from '@/components/checkbox'
-import * as fileInput from '@/components/file-input'
+// import * as fileInput from '@/components/_file-input'
 import * as input from '@/components/input'
 import * as link from '@/components/link'
 import * as progress from '@/components/progress'
@@ -22,12 +22,12 @@ import * as base from '@/base'
 const defaultPreset: Record<(typeof allComponents)[number], any> = {
   alert: alert.options,
   avatar: avatar.options,
-  bottomNavigation: bottomNavigation.options,
+  // bottomNavigation: bottomNavigation.options,
   button: button.options,
   badge: badge.options,
   chat: chat.options,
   checkbox: checkbox.options,
-  fileInput: fileInput.options,
+  // fileInput: fileInput.options,
   input: input.options,
   link: link.options,
   progress: progress.options,

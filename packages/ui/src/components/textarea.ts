4401b8b6b9d94bd391843673003494c4c1f86635
ice breaker <1324318532@qq.com>
2023-11-02 02:10:05 +0800

    sca

--- packages/ui/src/components/textarea.ts
+++ packages/ui/src/components/textarea.ts
@@ -1,57 +1,67 @@
-import { IDefaults, OptionFn, expandColorsMap } from './shared'
+import { OptionFn, getSelector, expandTypes } from './shared'
 
-const defaults: IDefaults = {
-  styled: {
-    default: 'border-base-content bg-base-100 rounded-btn border border-opacity-0',
-    bordered: 'border-opacity-20',
-    focus: 'outline-base-content/20 outline outline-2 outline-offset-2',
-    ghost: 'bg-opacity-5',
-    ghostFocus: {
-      apply: 'text-base-content bg-opacity-100',
-      css: {
-        'box-shadow': 'none'
-      }
-    },
-    disabled: 'border-base-200 bg-base-200 placeholder-base-content cursor-not-allowed text-opacity-20 placeholder-opacity-20'
-  },
-  base: {
-    default: 'min-h-12 flex-shrink px-4 py-2 text-sm leading-loose'
-  },
-  utils: {
-    sizes: {
-      xs: {
-        default: {
-          apply: 'px-2 py-1 text-xs leading-relaxed'
+export const options: OptionFn = (opts) => {
+  const { selector, types } = opts
+  return {
+    selector,
+    defaults: {
+      styled: {
+        [selector]: {
+          apply: 'border-base-content bg-base-100 rounded-btn border border-opacity-0',
+          '&-bordered': {
+            apply: 'border-opacity-20'
+          },
+          '&:focus': {
+            apply: 'outline-base-content/20 outline outline-2 outline-offset-2'
+          },
+          '&-ghost': {
+            apply: 'bg-opacity-5',
+            '&:focus': {
+              apply: 'text-base-content bg-opacity-100',
+              css: {
+                'box-shadow': 'none'
+              }
+            }
+          },
+          ...expandTypes(types, (type) => {
+            return {
+              key: `&${getSelector(type)}`,
+              value: {
+                apply: `border-${type}`,
+                '&:focus': {
+                  apply: `outline-${type}`
+                }
+              }
+            }
+          }),
+          [`&-disabled,
+          &:disabled,
+          &[disabled]`]: {
+            apply: 'border-base-200 bg-base-200 placeholder-base-content cursor-not-allowed text-opacity-20 placeholder-opacity-20'
+          }
         }
       },
-      sm: {
-        default: {
-          apply: 'px-3 py-1 text-sm leading-8'
+      base: {
+        [selector]: {
+          apply: 'min-h-12 flex-shrink px-4 py-2 text-sm leading-loose'
         }
       },
-      md: {
-        default: {
-          apply: 'px-4 py-3 text-sm leading-loose'
-        }
-      },
-      lg: {
-        default: {
-          apply: 'px-6 py-4 text-lg leading-loose'
+      utils: {
+        [selector]: {
+          [`&${getSelector('xs')}`]: {
+            apply: 'px-2 py-1 text-xs leading-relaxed'
+          },
+          [`&${getSelector('sm')}`]: {
+            apply: 'px-3 py-1 text-sm leading-8'
+          },
+          [`&${getSelector('md')}`]: {
+            apply: 'px-4 py-3 text-sm leading-loose'
+          },
+          [`&${getSelector('lg')}`]: {
+            apply: 'px-6 py-4 text-lg leading-loose'
+          }
         }
       }
     }
-  }
-}
-
-export const options: OptionFn = (opts) => {
-  return {
-    selector: '.textarea',
-    colors: expandColorsMap(opts.types, (cur) => {
-      return {
-        default: `border-${cur}`,
-        focus: `outline-${cur}`
-      }
-    }),
-    defaults
   }
 }
--- packages/ui/src/components/toggle.ts
+++ packages/ui/src/components/toggle.ts
@@ -1,116 +1,117 @@
-import { expandColorsMap, IDefaults, OptionFn } from './shared'
-
-const defaults: IDefaults = {
-  styled: {
-    default: {
-      apply: 'border-base-content bg-base-content rounded-badge h-6 w-12 cursor-pointer appearance-none border border-opacity-20 bg-opacity-50',
-      css: {
-        '--tglbg': 'rgba(var(--base-100))',
-        '--handleoffset': '1.5rem',
-        '--handleoffsetcalculator': 'calc(var(--handleoffset) * -1)',
-        '--togglehandleborder': '0 0',
-        transition: `background,
-        box-shadow var(--animation-input, 0.2s) ease-out`,
-        'box-shadow': `var(--handleoffsetcalculator) 0 0 2px var(--tglbg) inset,
-        0 0 0 2px var(--tglbg) inset,
-        var(--togglehandleborder)`
-      }
-    },
-    disabled: {
-      apply: 'border-base-content cursor-not-allowed bg-transparent opacity-30',
-      css: {
-        '--togglehandleborder': '0 0 0 3px rgba(var(--base-content)) inset, var(--handleoffsetcalculator) 0 0 3px rgba(var(--base-content)) inset'
-      }
-    },
-    focusVisible: {
-      apply: 'outline-base-content/20 outline outline-2 outline-offset-2'
-    },
-    checked: {
-      apply: 'border-opacity-100 bg-opacity-100',
-      css: {
-        '--handleoffsetcalculator': 'var(--handleoffset)'
-      }
-    },
-    indeterminate: {
-      apply: 'border-opacity-100 bg-opacity-100',
-      css: {
-        'box-shadow': `calc(var(--handleoffset) / 2) 0 0 2px var(--tglbg) inset,
-        calc(var(--handleoffset) / -2) 0 0 2px var(--tglbg) inset,
-        0 0 0 2px var(--tglbg) inset`
-      }
-    },
-    rtl: {
-      default: {
-        css: {
-          '--handleoffsetcalculator': 'calc(var(--handleoffset) * 1)'
-        }
-      },
-      checked: {
-        css: {
-          '--handleoffsetcalculator': 'calc(var(--handleoffset) * -1)'
-        }
-      },
-      indeterminate: {
-        css: {
-          'box-shadow': `calc(var(--handleoffset) / 2) 0 0 2px var(--tglbg) inset,
-          calc(var(--handleoffset) / -2) 0 0 2px var(--tglbg) inset,
-          0 0 0 2px var(--tglbg) inset`
-        }
-      }
-    }
-  },
-  base: {
-    default: {
-      apply: 'shrink-0'
-    }
-  },
-  utils: {
-    sizes: {
-      xs: {
-        default: {
-          apply: 'h-4 w-6',
-          css: {
-            '--handleoffset': '0.5rem'
-          }
-        }
-      },
-      sm: {
-        default: {
-          apply: 'h-5 w-8',
-          css: {
-            '--handleoffset': '0.75rem'
-          }
-        }
-      },
-      md: {
-        default: {
-          apply: 'h-6 w-12',
-          css: {
-            '--handleoffset': '1.5rem'
-          }
-        }
-      },
-      lg: {
-        default: {
-          apply: 'h-8 w-16',
-          css: {
-            '--handleoffset': '2rem'
-          }
-        }
-      }
-    }
-  }
-}
+import { OptionFn, getSelector, expandTypes } from './shared'
 
 export const options: OptionFn = (opts) => {
+  const { selector, types } = opts
   return {
-    selector: '.toggle',
-    colors: expandColorsMap(opts.types, (typeName) => {
-      return {
-        focusVisible: `outline-${typeName}`,
-        checked: `border-${typeName} bg-${typeName} text-${typeName}-content border-opacity-10`
+    selector,
+    defaults: {
+      styled: {
+        [selector]: {
+          apply: 'border-base-content bg-base-content rounded-badge h-6 w-12 cursor-pointer appearance-none border border-opacity-20 bg-opacity-50',
+          css: {
+            '--tglbg': 'rgba(var(--base-100))',
+            '--handleoffset': '1.5rem',
+            '--handleoffsetcalculator': 'calc(var(--handleoffset) * -1)',
+            '--togglehandleborder': '0 0',
+            transition: `background,
+            box-shadow var(--animation-input, 0.2s) ease-out`,
+            'box-shadow': `var(--handleoffsetcalculator) 0 0 2px var(--tglbg) inset,
+            0 0 0 2px var(--tglbg) inset,
+            var(--togglehandleborder)`
+          },
+          '[dir="rtl"] &': {
+            css: {
+              '--handleoffsetcalculator': 'calc(var(--handleoffset) * 1)'
+            }
+          },
+          '&:focus-visible': {
+            apply: 'outline-base-content/20 outline outline-2 outline-offset-2'
+          },
+          [`&:checked,
+          &[checked="true"],
+          &[aria-checked="true"]`]: {
+            apply: 'border-opacity-100 bg-opacity-100',
+            css: {
+              '--handleoffsetcalculator': 'var(--handleoffset)'
+            },
+            '[dir="rtl"] &': {
+              css: {
+                '--handleoffsetcalculator': 'calc(var(--handleoffset) * -1)'
+              }
+            }
+          },
+          '&:indeterminate': {
+            apply: 'border-opacity-100 bg-opacity-100',
+            css: {
+              'box-shadow': `calc(var(--handleoffset) / 2) 0 0 2px var(--tglbg) inset,
+            calc(var(--handleoffset) / -2) 0 0 2px var(--tglbg) inset,
+            0 0 0 2px var(--tglbg) inset`
+            },
+            '[dir="rtl"] &': {
+              css: {
+                'box-shadow': `calc(var(--handleoffset) / 2) 0 0 2px var(--tglbg) inset,
+                calc(var(--handleoffset) / -2) 0 0 2px var(--tglbg) inset,
+                0 0 0 2px var(--tglbg) inset`
+              }
+            }
+          },
+
+          ...expandTypes(types, (type) => {
+            return {
+              key: `&${getSelector(type)}`,
+              value: {
+                '&:focus-visible': {
+                  apply: `outline-${type}`
+                },
+                [`&:checked,
+                &[checked="true"],
+                &[aria-checked="true"]`]: {
+                  apply: `border-${type} bg-${type} text-${type}-content border-opacity-10`
+                }
+              }
+            }
+          }),
+          '&:disabled': {
+            apply: 'border-base-content cursor-not-allowed bg-transparent opacity-30',
+            css: {
+              '--togglehandleborder': '0 0 0 3px rgba(var(--base-content)) inset, var(--handleoffsetcalculator) 0 0 3px rgba(var(--base-content)) inset'
+            }
+          }
+        }
+      },
+      base: {
+        [selector]: {
+          apply: 'shrink-0'
+        }
+      },
+      utils: {
+        [selector]: {
+          [`&${getSelector('xs')}`]: {
+            apply: 'h-4 w-6',
+            css: {
+              '--handleoffset': '0.5rem'
+            }
+          },
+          [`&${getSelector('sm')}`]: {
+            apply: 'h-5 w-8',
+            css: {
+              '--handleoffset': '0.75rem'
+            }
+          },
+          [`&${getSelector('md')}`]: {
+            apply: 'h-6 w-12',
+            css: {
+              '--handleoffset': '1.5rem'
+            }
+          },
+          [`&${getSelector('lg')}`]: {
+            apply: 'h-8 w-16',
+            css: {
+              '--handleoffset': '2rem'
+            }
+          }
+        }
       }
-    }),
-    defaults
+    }
   }
 }

8d3ee85173da8aa38a0c574a240fc6ea68201fa3
ice breaker <1324318532@qq.com>
2023-11-01 21:22:45 +0800

    chore: as

--- packages/ui/src/components/checkbox.ts
+++ packages/ui/src/components/checkbox.ts
@@ -1,112 +1,118 @@
-import { IDefaults, OptionFn, expandColorsMap } from './shared'
-function generateDefault(typeName: string) {
-  return {
-    apply: `border-${typeName} [@media(hover:hover)]:hover:border-${typeName}`,
-    css: {
-      '--chkbg': `var(--${typeName})`,
-      '--chkfg': `var(--${typeName}-content)`
-    }
-  }
-}
+import { OptionFn, getSelector, expandTypes } from './shared'
 
-function generateFocus(typeName: string) {
-  return {
-    apply: `outline-${typeName}`
-  }
-}
-
-function generateChecked(typeName: string) {
-  return {
-    apply: `border-${typeName} bg-${typeName} text-${typeName}-content`
-  }
-}
-
-const defaults: IDefaults = {
-  styled: {
-    default: {
-      apply: 'border-base-content rounded-btn h-6 w-6 cursor-pointer appearance-none border border-opacity-20',
-      css: {
-        '--chkbg': 'var(--base-content)',
-        '--chkfg': 'var(--base-400)'
-      }
-    },
-    focusVisible: {
-      apply: 'outline-base-content outline outline-2 outline-offset-2'
-    },
-    checked: {
-      apply: 'bg-base-content bg-no-repeat',
-      css: {
-        animation: 'checkmark var(--animation-input, 0.2s) ease-out',
-        'background-image': `linear-gradient(-45deg, transparent 65%, rgba(var(--chkbg)) 65.99%),
-        linear-gradient(45deg, transparent 75%, rgba(var(--chkbg)) 75.99%),
-        linear-gradient(-45deg, rgba(var(--chkbg)) 40%, transparent 40.99%),
-        linear-gradient(
-          45deg,
-          rgba(var(--chkbg)) 30%,
-          rgba(var(--chkfg)) 30.99%,
-          rgba(var(--chkfg)) 40%,
-          transparent 40.99%
-        ),
-        linear-gradient(-45deg, rgba(var(--chkfg)) 50%, rgba(var(--chkbg)) 50.99%)`
-      }
-    },
-    indeterminate: {
-      apply: 'bg-base-content bg-no-repeat',
-      css: {
-        animation: 'checkmark var(--animation-input, 0.2s) ease-out',
-        'background-image': `linear-gradient(90deg, transparent 80%, rgba(var(--chkbg)) 80%),
-        linear-gradient(-90deg, transparent 80%, rgba(var(--chkbg)) 80%),
-        linear-gradient(
-          0deg,
-          rgba(var(--chkbg)) 43%,
-          rgba(var(--chkfg)) 43%,
-          rgba(var(--chkfg)) 57%,
-          rgba(var(--chkbg)) 57%
-        )`
-      }
-    },
-    disabled: 'bg-base-content cursor-not-allowed border-transparent opacity-20'
-  },
-  base: {
-    default: {
-      apply: 'shrink-0'
-    }
-  },
-  utils: {
-    sizes: {
-      xs: {
-        default: {
-          apply: 'h-4 w-4'
-        }
-      },
-      sm: {
-        default: {
-          apply: 'h-5 w-5'
-        }
-      },
-      md: {
-        default: {
-          apply: 'h-6 w-6'
-        }
-      },
-      lg: {
-        default: {
-          apply: 'h-8 w-8'
-        }
-      }
-    }
-  }
-}
 export const options: OptionFn = (opts) => {
+  const { selector, types } = opts
   return {
-    selector: '.checkbox',
-    colors: expandColorsMap(opts.types, (cur) => {
-      return {
-        default: generateDefault(cur),
-        focusVisible: generateFocus(cur),
-        checked: generateChecked(cur)
+    selector,
+    defaults: {
+      styled: {
+        [selector]: {
+          apply: 'border-base-content rounded-btn h-6 w-6 cursor-pointer appearance-none border border-opacity-20',
+          css: {
+            '--chkbg': 'var(--base-content)',
+            '--chkfg': 'var(--base-400)'
+          },
+          '&:focus-visible': {
+            apply: 'outline-base-content outline outline-2 outline-offset-2'
+          },
+          [`&:checked,
+          &[checked="true"],
+          &[aria-checked="true"]`]: {
+            apply: 'bg-base-content bg-no-repeat',
+            css: {
+              animation: 'checkmark var(--animation-input, 0.2s) ease-out',
+              'background-image': `linear-gradient(-45deg, transparent 65%, rgba(var(--chkbg)) 65.99%),
+              linear-gradient(45deg, transparent 75%, rgba(var(--chkbg)) 75.99%),
+              linear-gradient(-45deg, rgba(var(--chkbg)) 40%, transparent 40.99%),
+              linear-gradient(
+                45deg,
+                rgba(var(--chkbg)) 30%,
+                rgba(var(--chkfg)) 30.99%,
+                rgba(var(--chkfg)) 40%,
+                transparent 40.99%
+              ),
+              linear-gradient(-45deg, rgba(var(--chkfg)) 50%, rgba(var(--chkbg)) 50.99%)`
+            }
+          },
+          '&:indeterminate': {
+            apply: 'bg-base-content bg-no-repeat',
+            css: {
+              animation: 'checkmark var(--animation-input, 0.2s) ease-out',
+              'background-image': `linear-gradient(90deg, transparent 80%, rgba(var(--chkbg)) 80%),
+          linear-gradient(-90deg, transparent 80%, rgba(var(--chkbg)) 80%),
+          linear-gradient(
+            0deg,
+            rgba(var(--chkbg)) 43%,
+            rgba(var(--chkfg)) 43%,
+            rgba(var(--chkfg)) 57%,
+            rgba(var(--chkbg)) 57%
+          )`
+            }
+          },
+          ...expandTypes(types, (type) => {
+            return {
+              key: `&${getSelector(type)}`,
+              value: {
+                apply: `border-${type} [@media(hover:hover)]:hover:border-${type}`,
+                css: {
+                  '--chkbg': `var(--${type})`,
+                  '--chkfg': `var(--${type}-content)`
+                },
+                '&:focus-visible': {
+                  apply: `outline-${type}`
+                },
+                [`&:checked,
+          &[checked="true"],
+          &[aria-checked="true"]`]: {
+                  apply: `border-${type} bg-${type} text-${type}-content`
+                }
+              }
+            }
+          }),
+
+          '&:disabled': {
+            apply: 'bg-base-content cursor-not-allowed border-transparent opacity-20'
+          }
+        }
+        // '@keyframes checkmark': {
+        //   '0%': {
+        //     css: {
+        //       'background-position-y': '5px'
+        //     }
+        //   },
+        //   '50%': {
+        //     css: {
+        //       'background-position-y': '-2px'
+        //     }
+        //   },
+        //   '100%': {
+        //     css: {
+        //       'background-position-y': '0'
+        //     }
+        //   }
+        // }
+      },
+      base: {
+        [selector]: {
+          apply: 'shrink-0'
+        }
+      },
+      utils: {
+        [selector]: {
+          [`&${getSelector('xs')}`]: {
+            apply: 'h-4 w-4'
+          },
+          [`&${getSelector('sm')}`]: {
+            apply: 'h-5 w-5'
+          },
+          [`&${getSelector('md')}`]: {
+            apply: 'h-6 w-6'
+          },
+          [`&${getSelector('lg')}`]: {
+            apply: 'h-8 w-8'
+          }
+        }
       }
-    }),
-    defaults
+    }
   }
 }
--- packages/ui/src/components/input.ts
+++ packages/ui/src/components/input.ts
@@ -1,80 +1,78 @@
-import { IDefaults, OptionFn, expandColorsMap } from './shared'
+import { OptionFn, getSelector, expandTypes } from './shared'
 
-function generateDefault(typeName: string) {
-  return {
-    apply: `border-${typeName}`
-  }
-}
-
-function generateFocus(typeName: string) {
-  return {
-    apply: `shadow-${typeName}/10` // outline-${typeName}`
-  }
-}
-
-const defaults: IDefaults = {
-  styled: {
-    default: {
-      apply: 'border-base-content bg-base-100 rounded-btn border border-opacity-0 text-base'
-    },
-    bordered: {
-      apply: 'border-opacity-20'
-    },
-    focus: {
-      apply: 'outline-0 shadow-[0_0_0_2px] shadow-base-content/10' //  outline-base-content/20 outline outline-1 outline-offset-1'
-    },
-    ghost: {
-      apply: 'bg-opacity-5'
-    },
-    ghostFocus: {
-      apply: 'text-base-content bg-opacity-100',
-      css: {
-        'box-shadow': 'none'
-      }
-    },
-    disabled: {
-      apply: 'border-base-200 bg-base-200 placeholder-base-content cursor-not-allowed text-opacity-20 placeholder-opacity-20'
-    }
-  },
-  base: {
-    default: {
-      apply: 'flex-shrink h-8 px-3 text-sm leading-loose'
-    }
-  },
-  utils: {
-    sizes: {
-      xs: {
-        default: {
-          apply: 'h-6 px-2 text-xs leading-relaxed'
-        }
-      },
-      sm: {
-        default: {
-          apply: 'h-8 px-3 text-sm leading-8'
-        }
-      },
-      md: {
-        default: {
-          apply: 'h-12 px-4 text-sm leading-loose'
-        }
-      },
-      lg: {
-        default: {
-          apply: 'h-16 px-6 text-lg leading-loose'
-        }
-      }
-    }
-  }
-}
 export const options: OptionFn = (opts) => {
+  const { selector, types } = opts
   return {
-    selector: '.input',
-    colors: expandColorsMap(opts.types, (cur) => {
-      return {
-        default: generateDefault(cur),
-        focus: generateFocus(cur)
+    selector,
+    defaults: {
+      styled: {
+        [selector]: {
+          apply: 'border-base-content bg-base-100 rounded-btn border border-opacity-0 text-base',
+          'input:focus': {
+            apply: 'outline-none'
+          },
+          '&[list]::-webkit-calendar-picker-indicator': {
+            css: {
+              'line-height': '1em'
+            }
+          },
+          '&-bordered': {
+            apply: 'border-opacity-20'
+          },
+          [`&:focus,
+          &:focus-within`]: {
+            apply: 'outline-0 shadow-[0_0_0_2px] shadow-base-content/10'
+          },
+          '&-ghost': {
+            apply: 'bg-opacity-5',
+            [`&:focus,
+            &:focus-within`]: {
+              apply: 'text-base-content bg-opacity-100',
+              css: {
+                'box-shadow': 'none'
+              }
+            }
+          },
+          ...expandTypes(types, (type) => {
+            return {
+              key: `&${getSelector(type)}`,
+              value: {
+                apply: `border-${type}`,
+                [`&:focus,
+            &:focus-within`]: {
+                  apply: `shadow-${type}/10`
+                }
+              }
+            }
+          }),
+          [`&-disabled,
+          &:disabled,
+          &[disabled]`]: {
+            apply: 'border-base-200 bg-base-200 placeholder-base-content cursor-not-allowed text-opacity-20 placeholder-opacity-20'
+          }
+        }
+      },
+      base: {
+        [selector]: {
+          apply: 'flex-shrink h-8 px-3 text-sm leading-loose'
+        }
+      },
+      utils: {
+        [selector]: {
+          [`&${getSelector('xs')}`]: {
+            apply: 'h-6 px-2 text-xs leading-relaxed'
+          },
+          [`&${getSelector('sm')}`]: {
+            apply: 'h-8 px-3 text-sm leading-8'
+          },
+          [`&${getSelector('md')}`]: {
+            apply: 'h-12 px-4 text-sm leading-loose'
+          },
+          [`&${getSelector('lg')}`]: {
+            apply: 'h-16 px-6 text-lg leading-loose'
+          }
+        }
       }
-    }),
-    defaults
+    }
   }
 }

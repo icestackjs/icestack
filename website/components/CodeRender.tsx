985731f5f3cf9e80de0b69c63dbdaa72ed53bede
ice breaker <1324318532@qq.com>
2023-12-24 17:46:37 +0800

    chore: commit docs

--- website/components/CodeRender.tsx
+++ website/components/CodeRender.tsx
@@ -1,28 +1,6 @@
-// import reactElementToJSXString from 'react-element-to-jsx-string'
 import { PropsWithChildren } from 'react'
-// import { trim } from 'lodash-es'
 import parse from 'html-react-parser'
-// function matchAll(regex: RegExp, str: string) {
-//   const array: RegExpExecArray[] = []
-//   let arr: RegExpExecArray
-//   while ((arr = regex.exec(str)) !== null) {
-//     array.push(arr)
-//   }
-//   return array
-// }
-// code ?? (
-//   <div dangerouslySetInnerHTML={{ __html: '\n<div>12</div>\n' }}>
-//     {/* {arr.map((x, i) => {
-//     return <div key={i} dangerouslySetInnerHTML={{ __html: x[1] }}></div>
-//   })} */}
-//   </div>
-// )
 
-// const regex = /```html(.*?)```/gms
-// const mdRaw: string = props.code //  reactElementToJSXString(props.children)
-// console.log(mdRaw)
-// const arr = matchAll(regex, mdRaw)
-// const XXX =
 export default function Index({ code }: PropsWithChildren<{ code?: string }>) {
-  return parse(code)
+  return <div className="flex border-base-300 border rounded min-h-[6rem] min-w-[18rem] max-w-4xl flex-wrap items-center justify-center p-4">{parse(code)}</div>
 }

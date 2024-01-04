d8cfc39d7aec778b15d11b992c29c6b29f59240a
ice breaker <1324318532@qq.com>
2024-01-04 22:47:39 +0800

    docs: update unocss plugin

--- examples/unocss-sample-app/uno.config.ts
+++ examples/unocss-sample-app/uno.config.ts
@@ -3,13 +3,11 @@ import path from 'node:path'
 import { defineConfig, presetUno } from 'unocss'
 import { icestackPreset } from '@icestack/unocss'
 
-// const replacePrefix = (css: string) => css.replaceAll('--tw-', '--un-')
-
 export default defineConfig({
   presets: [
     icestackPreset({
-      loadConfig: true,
-      loadDirectory: path.resolve(__dirname, 'my-ui')
+      loadDirectory: path.resolve(__dirname, 'my-ui'),
+      loadConfig: true
     }),
     presetUno()
   ]

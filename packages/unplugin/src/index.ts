ccb1eb2664e991fc8440b40d9fc7099b8607b339
ice breaker <1324318532@qq.com>
2024-01-08 17:37:35 +0800

    chore: fix unplugin build errot

--- packages/unplugin/src/index.ts
+++ packages/unplugin/src/index.ts
@@ -1,18 +1,13 @@
 import { createUnplugin } from 'unplugin'
 
-export interface UserOptions {}
-
-export const unplugin = createUnplugin((options: UserOptions) => {
+export const unplugin = createUnplugin(() => {
   return {
-    name: 'icestack:unplugin',
-
-    resolveId(id) {},
-    transform(code) {}
+    name: 'icestack:unplugin'
   }
 })
 
-export const vitePlugin = unplugin.vite
-export const rollupPlugin = unplugin.rollup
-export const webpackPlugin = unplugin.webpack
-export const rspackPlugin = unplugin.rspack
-export const esbuildPlugin = unplugin.esbuild
+// export const vitePlugin = unplugin.vite
+// export const rollupPlugin = unplugin.rollup
+// export const webpackPlugin = unplugin.webpack
+// export const rspackPlugin = unplugin.rspack
+// export const esbuildPlugin = unplugin.esbuild

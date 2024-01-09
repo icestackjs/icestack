7a5d7813e4aedccc7aae6ec1658a259183ca6498
ice breaker <1324318532@qq.com>
2024-01-09 14:52:32 +0800

    chore: commit @icestack/cli

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,108 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { Command } from 'commander'
+import chokidar from 'chokidar'
+import dedent from 'dedent'
+import { loadSync } from '@icestack/config'
+import { logger } from '@icestack/logger'
+import { createContext } from '@icestack/core'
+
+export const cli = new Command()
+
+async function letUsBuild(options: { clean?: boolean; configFile?: string } = {}) {
+  const { clean, configFile } = options
+  if (configFile) {
+    logger.success(`load config from ${configFile}`)
+  }
+  const { config } = await loadSync({
+    configFile
+  })
+  if (config) {
+    if (!config.outdir) {
+      logger.error('outdir option must be passed!')
+      return
+    }
+    if (clean || config.clean) {
+      const { deleteAsync } = await import('del')
+      await deleteAsync(config.outdir)
+      logger.success(`del outdir: ${config.outdir} successfully!`)
+    }
+
+    const ctx = createContext(config)
+    await ctx.build()
+    logger.success('build successfully!')
+  }
+}
+const defaultOutdir = 'my-ui'
+
+function createJsConfig(outdir: string, format: 'cjs' | 'ts' = 'cjs') {
+  const p = './' + outdir
+  if (format === 'ts') {
+    return {
+      filename: 'icestack.config.ts',
+      data: dedent`import { defineConfig } from '@icestack/ui'\n\nexport default defineConfig({
+        outdir: '${p}'
+      })\n`
+    }
+  }
+  return {
+    filename: 'icestack.config.cjs',
+    data: dedent`/**\n * @type {import('@icestack/ui').Config}\n */\nconst config = {
+      outdir: '${p}'
+    }\n\n${'module.exports = config'}\n`
+  }
+}
+
+cli
+  .command('init')
+  .description('init config')
+  .option('--only-config', 'output onlt config')
+  .option('--format <format>', 'config file format')
+  .option('--outdir <outdir>')
+  .action(async (options) => {
+    const cwd = process.cwd()
+    const { onlyConfig, outdir = defaultOutdir, format = 'cjs' } = options
+    const { data, filename } = createJsConfig(outdir, format)
+    const p = path.resolve(cwd, filename)
+    fs.writeFileSync(p, data, 'utf8') // ts ? tsT : jsT)
+    logger.success(`init ${filename} successfully!`)
+    if (onlyConfig) {
+      return
+    }
+    const gitignoreFilePath = path.resolve(cwd, '.gitignore')
+    fs.existsSync(gitignoreFilePath) ? fs.appendFileSync(gitignoreFilePath, `\n${outdir}\n`, 'utf8') : fs.writeFileSync(gitignoreFilePath, `${outdir}\n`, 'utf8')
+
+    await letUsBuild()
+  })
+
+cli
+  .command('build')
+  .description('code generate')
+  .option('--clean')
+  .option('-c, --config <path>', 'config path')
+  .action(async (options) => {
+    const { clean, config } = options
+    const opts = { clean, configFile: config }
+    await letUsBuild(opts)
+  })
+
+cli
+  .command('watch')
+  .description('watch config file change and build lib')
+  .option('-c, --config <path>', 'config path')
+  .action((options) => {
+    const { clean, config } = options
+    const opts = { clean, configFile: config }
+    const cwd = process.cwd()
+
+    chokidar
+      .watch(path.resolve(cwd, 'icestack.config.{js,ts,cjs}'))
+      .on('change', async () => {
+        logger.success('some changes happened')
+        await letUsBuild(opts)
+      })
+      .on('ready', async () => {
+        await letUsBuild(opts)
+        logger.success('start watching config......')
+      })
+  })
